5a194f1e891684c82ebfd10b323683451f9beb92
Joao Gaspar <danjpjcisalh@gmail.com>
2026-07-16 17:59:01 -0300

    style(frontend): adiciona layout geral e estrutura em secoes no App

--- frontend/controle-gastos/src/App.tsx
+++ frontend/controle-gastos/src/App.tsx
@@ -8,37 +8,67 @@ import { buscarTransacoes } from './services/TransacaoService';
 import type { Transacao } from './types/Transacao';
 import { ListaTransacoes } from './components/ListaTransacoes';
 import { FormularioTransacao } from './components/FormularioTransacao';
+import { TabelaTotais } from './components/TabelaTotais';
+import { buscarRelatorio } from './services/RelatorioService';
+import type { RelatorioTotais } from './types/Relatorio';
 
 function App() {
   const [pessoas, setPessoas] = useState<Pessoa[]>([]);
   const [transacoes, setTransacoes] = useState<Transacao[]>([]);
+  const [relatorio, setRelatorio] = useState<RelatorioTotais | null>(null);
+
   const carregarPessoas = async () => {
     const dados = await buscarPessoas();
+    await carregarRelatorio();
     setPessoas(dados);
-  };
+    };
 
   useEffect(()=> {
     carregarPessoas();
+    carregarRelatorio();
+    carregarTransacoes();
   }, []);
 
   const handleDeletar = async (id: number) => {
     await deletarPessoa(id);
     carregarPessoas();
     carregarTransacoes();
+    carregarRelatorio();
   };
 
   const carregarTransacoes = async () => {
     const dados = await buscarTransacoes();
     setTransacoes(dados);
   };
+  const handleCarregarTransacoes = async () => {
+    await carregarPessoas();
+    await carregarTransacoes();
+    await carregarRelatorio();
+  }
+
+  const carregarRelatorio = async () => {
+    const dados = await buscarRelatorio();
+    setRelatorio(dados);
+  }
 
   return (
     <>
-      <h1>Controle Gastos</h1>
-      <FormularioPessoa aoCriar={carregarPessoas}/>
-      <ListaPessoas pessoas={pessoas} aoDeletar={handleDeletar}/>
-      <FormularioTransacao pessoas={pessoas} aoCriar={carregarTransacoes} />
-      <ListaTransacoes transacoes={transacoes} />
+      <h1 className='titulo'>Controle Gastos</h1>
+      <section>
+        <h2>Pessoas</h2>
+        <FormularioPessoa aoCriar={carregarPessoas}/>
+        <ListaPessoas pessoas={pessoas} aoDeletar={handleDeletar}/>
+      </section>
+
+      <section>
+        <h2>Transações</h2>
+        <FormularioTransacao pessoas={pessoas} aoCriar={handleCarregarTransacoes} />
+        <ListaTransacoes transacoes={transacoes} />
+      </section>
+      <section>
+        <h2>Totais</h2>
+        <TabelaTotais relatorio={relatorio}  />
+      </section>
     </>
   )
 }
